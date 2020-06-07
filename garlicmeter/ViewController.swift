57c274614c20da04b71775e86d01a4ad5e9b21f2
tiking76 <716yosiki1999@gmail.com>
2020-06-07 09:54:46 +0900

    initial commit

---
+++ garlicmeter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  garlicmeter
+//
+//  Created by 舘佳紀 on 2020/06/07.
+//  Copyright © 2020 yoshiki Tachi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
