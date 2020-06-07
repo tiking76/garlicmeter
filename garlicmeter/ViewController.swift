bac07d32a78fdadbe9b86061bda62073fe02d5a1
tiking76 <716yosiki1999@gmail.com>
2020-06-07 15:36:08 +0900

    prototype

--- garlicmeter/ViewController.swift
+++ garlicmeter/ViewController.swift
@@ -7,14 +7,63 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var garlicImage: UIImageView!
+    
+    @IBOutlet weak var ManyButton: UIButton!
+    
+    @IBOutlet weak var NormalButton: UIButton!
+    
+    @IBOutlet weak var SmallButton: UIButton!
+    
+    @IBOutlet weak var TextLabel: UILabel!
+    
+    private let disposeBeg = DisposeBag()
+    private var garlicCount : Double = 0
+    let userDefaults = UserDefaults.standard
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        TextLabel.text = "累計\(garlicCount)片"
+
+
+        ManyButton.rx.tap
+                .subscribe(onNext: { [weak self] in
+                    guard let self = self else { return }
+                    self.garlicCount += 2
+                    self.userDefaults.set(self.garlicCount, forKey: "garlicCount")
+                    self.garlicCount = self.userDefaults.double(forKey: "garlicCount")
+                    self.TextLabel.text = "累計\(self.garlicCount)片"
+                    self.garlicCount = self.userDefaults.object(forKey: "garlicCount") as! Double
+                })
+                .disposed(by: disposeBeg)
+
+        NormalButton.rx.tap
+                .subscribe(onNext: { [weak self] in
+                    guard let self = self else { return }
+                    self.garlicCount += 1
+                    self.userDefaults.set(self.garlicCount, forKey: "garlicCount")
+                    self.garlicCount = self.userDefaults.double(forKey: "garlicCount")
+                    self.TextLabel.text = "累計\(self.garlicCount)片"
+                    self.garlicCount = self.userDefaults.object(forKey: "garlicCount") as! Double
+                })
+                .disposed(by: disposeBeg)
+
+        SmallButton.rx.tap
+                .subscribe(onNext: {[weak self] in
+                    guard let self = self else { return }
+                    self.garlicCount += 0.5
+                    self.userDefaults.set(self.garlicCount, forKey: "garlicCount")
+                    self.garlicCount = self.userDefaults.double(forKey: "garlicCount")
+                    self.TextLabel.text = "累計\(self.garlicCount)片"
+                    self.garlicCount = self.userDefaults.object(forKey: "garlicCount") as! Double
+                })
+                .disposed(by: disposeBeg)
     }
-
-
 }
 
